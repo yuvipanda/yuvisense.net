7d1ae1b09430e8c0c94008607aba2fc87f383f0e
yuvipanda <yuvipanda@gmail.com>
2009-07-31 21:01:22 +0530

    removed some themes

--- themes/freestyle/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	
-	$("ul#top-nav li:first").addClass('first-li');
-	$(".left-sidebar:first").addClass('first-left-sidebar');
-	$(".right-sidebar:first").addClass('first-right-sidebar');
-	$("ul.post-meta li:last-child").addClass('last-post-meta-li');
-	$("blockquote p:last-child").addClass('last-blockquote-p');
-	
-});
