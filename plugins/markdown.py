6be85d7b3359b26e18e1f0c514452552d225c272
Yuvi Panda <yuvipanda@gmail.com>
2009-10-14 00:45:02 +0530

    BS

--- plugins/markdown.py
+++ plugins/markdown.py
@@ -1,17 +1,37 @@
-from markdown import markdown
+from markdown import Markdown
 import os
+import yaml
 
 css = '<link rel="stylesheet" type="text/css" href="<base_url>static/codehilite.css" />'
+
+autolinks = {}
+
+
+def wikilink_transform(label, base, end):
+    if ' ' in label:
+        key = label.split()[0]
+        text = ' '.join(label.split()[1:])
+    else:        
+        key = text = label
+    href = key
+    return <a href="%s">%s</a>' % (href, text)
+
+md = Markdown(extensions=['codehilite', 'wikilinks'],
+              extension_configs={'wikilinks':[('build_url', wikilink_transform)]})
+
 
 def transform_markdown(post):
     if 'content-type' in post.meta and post.meta['content-type'] == 'XHTML':
         return post.content
     else:
-        if not hasattr(post, 'content_html'):
-            post.content_html = markdown(post.content, ['codehilite'])
+        if not hasattr(post, 'content_html'):          
+            post.content_html = md.convert(post.content)
+                                               
         return post.content_html
 
 def main(blog):
+    autolinks_path = os.path.join(blog.settings['blog_dir'], 'autolink.yaml')    
+    autolinks = yaml.load(open(autolinks_path))
     blog.hooks.add_action('render',transform_markdown)
     blog.hooks.copy_to_static(os.path.join('plugins','codehilite.css'))
     blog.hooks.add_action('html_head', 
