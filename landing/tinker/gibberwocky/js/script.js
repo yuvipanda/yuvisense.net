a1877b262a2466c556d524195964eedee68f2e68
Yuvaraj Pandian T <yuvipanda@gmail.com>
2010-12-08 22:41:49 +0530

    More levels for the gibberwocky script

--- landing/tinker/gibberwocky/js/script.js
+++ landing/tinker/gibberwocky/js/script.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
         $("#go").click(function(){
                 google.language.translate($("#text").val(), "en", "es", function(result) {
                     google.language.translate(result.translation, "es", "nl", function(result) {
-                        google.language.translate(result.translation, "nl", "en", function(result) {
-                            $("#translated").text(result.translation);
+                        google.language.translate(result.translation, "nl", "el", function(result) {
+                            google.language.translate(result.translation, "el", "de", function(result) {
+                                google.language.translate(result.translation, "de", "no", function(result) {
+                                    google.language.translate(result.translation, "no", "en", function(result) {
+                                $("#translated").text(result.translation);
+                                });
+                                });
+                                });
                             });
                     });
                 });
