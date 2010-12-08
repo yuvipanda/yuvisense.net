47b3ce9334ac9401cb2c695719341e58d610a689
Yuvaraj Pandian T <yuvipanda@gmail.com>
2010-12-09 04:23:31 +0530

    Minor Animations

--- landing/tinker/gibberwocky/js/script.js
+++ landing/tinker/gibberwocky/js/script.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function(){
+        var translated = $("#translated");
         $("#go").click(function(){
+            translated.fadeOut(400, function() {
+            translated.html("<em>GibberWockifying...</em>");
+            translated.fadeIn(400);});
                 google.language.translate($("#text").val(), "en", "es", function(result) {
                     google.language.translate(result.translation, "es", "nl", function(result) {
                         google.language.translate(result.translation, "nl", "el", function(result) {
@@ -8,7 +12,9 @@ $(document).ready(function(){
                                 google.language.translate(result.translation, "de", "no", function(result) {
                                     google.language.translate(result.translation, "no", "ru", function(result) {
                                         google.language.translate(result.translation, "ru", "en", function(result) {
-                                            $("#translated").html(result.translation);
+                                            translated.fadeOut(400, function() {
+                                            translated.html(result.translation);
+                                            translated.fadeIn(400); });
                                 });
                                 });
                                 });
