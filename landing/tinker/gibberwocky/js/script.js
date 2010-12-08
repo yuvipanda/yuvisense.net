98edec001591ff10568c1cf61229fd7cc9e629ad
Yuvaraj Pandian T <yuvipanda@gmail.com>
2010-12-08 22:34:41 +0530

    Modified language strings

--- landing/tinker/gibberwocky/js/script.js
+++ landing/tinker/gibberwocky/js/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
         $("#go").click(function(){
                 google.language.translate($("#text").val(), "en", "es", function(result) {
-                    google.language.translate(result.translation, "es", "fi", function(result) {
-                        google.language.translate(result.translation, "fi", "en", function(result) {
+                    google.language.translate(result.translation, "es", "nl", function(result) {
+                        google.language.translate(result.translation, "nl", "en", function(result) {
                             $("#translated").text(result.translation);
                             });
                     });
